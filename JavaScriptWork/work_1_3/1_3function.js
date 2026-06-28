4697caa9b021f451666495f7ec8a1b2b325566b1
tsunoda <yuumani508256k30@gmail.com>
2026-06-28 16:23:33 +0900

    関数課題を追加

---
+++ JavaScriptWork/work_1_3/1_3function.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+    console.log("こんにちは！"+name + "さん");
+    
+}
+
+sayHello("角田")
+
+const getTotalPrice = (price, quantity) => price * quantity;
+const addTax =total => Math.floor(total * 1.1);
+
+const total = getTotalPrice(1000, 2); // → 2000
+console.log(`税抜金額は${total}円です`) ;
+
+const taxedTotal = addTax(total); // → 2200
+console.log (`税込金額は${taxedTotal}円です`)
+
+
